0bdab5393e53a4128943f6f1bce000242631a37a
jadehaus <minu.kim@kaist.ac.kr>
2024-09-08 14:29:19 +0900

    update trained models for atari

--- reinforcement_learning.py
+++ reinforcement_learning.py
@@ -1,34 +1,55 @@
 import gymnasium as gym
 
-from stable_baselines3 import PPO
-from stable_baselines3 import A2C
 from stable_baselines3.common.env_util import make_atari_env
 from stable_baselines3.common.vec_env import VecFrameStack
+from stable_baselines3 import PPO, DQN
 
-env = gym.make("BipedalWalker-v3", render_mode="human")
-# env = make_atari_env("BreakoutNoFrameskip-v4", n_envs=4, seed=0)
-# env = VecFrameStack(env, n_stack=4)
+# Customize your training settings here
+ENV_NAME = ???
+TRAINED_TOTAL_TIMESTEPS = ???
+ALGORITHM = ???
+POLICY = ???
 
-# Learn yourself or...
-model = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=10_000)
+# Environment name mapping
+# Add your own version mapping for convenience
+ATARI_ENVS = [
+    'Boxing',
+    'Breakout',
+]
+ENV_VERSION_MAPPING = {
+    'BipedalWalker': '-v3',
+    'Boxing': 'NoFrameskip-v4',
+    'Breakout': 'NoFrameskip-v4',
+}
 
-# model = A2C("CnnPolicy", env, verbose=1)
-# model.learn(total_timesteps=10_000)
+version = ENV_VERSION_MAPPING[ENV_NAME]
 
-# load from checkpoint
-# checkpoint = load_from_hub(
-# 	repo_id="sb3/ppo-BipedalWalker-v3",
-# 	filename="{MODEL FILENAME}.zip",
-# )
-# model = PPO.load(checkpoint)
+if ENV_NAME in ATARI_ENVS:
+    env = make_atari_env(f"{ENV_NAME}{version}", n_envs=1)
+    env = VecFrameStack(env, n_stack=1)
+else:
+    env = gym.make(f"{ENV_NAME}{version}", render_mode='human')
 
-print("Evaluating")
-env = model.get_env()
+try:
+    timesteps_str = f'{TRAINED_TOTAL_TIMESTEPS // 1000}k'
+    file_path = f'./trained_weights/{ENV_NAME}_{ALGORITHM.__name__}_{POLICY}_{timesteps_str}'
+    model = ALGORITHM.load(file_path, env, verbose=1)
+    print(f"Loaded trained models from checkpoint {file_path}.zip.")
+except FileNotFoundError:
+    print(f"Trained model not found. Training from scratch..")
+    model = ALGORITHM(POLICY, env, verbose=1)
+    model.learn(total_timesteps=TRAINED_TOTAL_TIMESTEPS)
+    model.save(file_path)
+
+if ENV_NAME not in ATARI_ENVS:
+    env = model.get_env()
+
 obs = env.reset()
-for i in range(1000):
-    action, _states = model.predict(obs, deterministic=True)
-    obs, reward, done, info = env.step(action)
+total_rewards = 0
+for _ in range(1000):
+    action, _states = model.predict(obs, deterministic=False)
+    obs, rewards, dones, info = env.step(action)
     env.render("human")
-
-env.close()
+    total_rewards += rewards
+    
+print(f"Total Return: {total_rewards}")
---
+++ trained_weights/README.md
@@ -0,0 +1,5 @@
+### Trained Model Parameters
+
+Download from the following link:
+
+https://drive.google.com/drive/u/0/folders/1wDf4F46SiH23wFHZ6lO79FZymcETLO5n
